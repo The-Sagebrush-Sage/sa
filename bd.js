34d8ef2f5bb526dab1827f0716c5d9e85d6b1345
The-Sagebrush-Sage <Wermut-Kraft@yandex.ru>
2025-12-25 21:33:31 +0300

    Add files via upload

--- bd.js
+++ bd.js
@@ -1,24 +1,25 @@
 const userDatabase = {
     "user_5sjr50": {
-        username: "222",
-        profileName: "user_5sjr50",
-        profileNickname: "555",
-        profileStyle: "Дизайн",
-        avatar: "https://i.postimg.cc/6p6LLrLR/photo_5244943526083553957_y.jpg",
-        banner: "https://i.postimg.cc/8zGLmfKb/image.png", // ← ДОБАВЬТЕ БАННЕР ДЛЯ ТЕСТА
-        socialLinks: [
-            { platform: "telegram", username: "234" },
-            { platform: "telegram", username: "123456" }
-        ],
-        works: [
-            "https://i.postimg.cc/Vk3Hs0tN/photo-5271895704291372799-c.jpg",
-            "https://i.postimg.cc/Vk3Hs0tN/photo-5271895704291372799-c.jpg",
-            "https://i.postimg.cc/Vk3Hs0tN/photo-5271895704291372799-c.jpg",
-            "https://i.postimg.cc/VvczXRxZ/photo_5337314107818374848_c.jpg",
-            "https://i.postimg.cc/Vk3Hs0tN/photo-5271895704291372799-c.jpg"
-        ],
-        publishedOn: "prem_artist",
-        password: "123456",
-        profileDescription: "",
-    },
+    username: "user_5sjr50",
+    profileName: "user_5sjr50",
+    profileNickname: "555",
+    profileStyle: "Дизайн",
+    avatar: "https://i.postimg.cc/6p6LLrLR/photo_5244943526083553957_y.jpg",
+    banner: "https://i.postimg.cc/8zGLmfKb/image.png", // ← ДОБАВЬТЕ БАННЕР В БАЗУ ДАННЫХ
+    socialLinks: [
+        { platform: "telegram", username: "234" },
+        { platform: "telegram", username: "123456" }
+    ],
+    works: [
+        "https://i.postimg.cc/Vk3Hs0tN/photo-5271895704291372799-c.jpg",
+        "https://i.postimg.cc/VvczXRxZ/photo_5337314107818374848_c.jpg",
+        "https://i.postimg.cc/Vk3Hs0tN/photo-5271895704291372799-c.jpg",
+        "https://i.postimg.cc/Vk3Hs0tN/photo-5271895704291372799-c.jpg",
+        "https://i.postimg.cc/Vk3Hs0tN/photo-5271895704291372799-c.jpg"
+    ],
+    publishedOn: "design",
+    password: "123456",
+    profileDescription: "",
+    banner: "https://i.postimg.cc/8zGLmfKb/image.png", // ← ЭТО ВАЖНОЕ ДОБАВЛЕНИЕ
+},
 }
