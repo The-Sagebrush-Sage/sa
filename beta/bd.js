f5171bab8c6184e957c169ed90389632fe381053
The-Sagebrush-Sage <Wermut-Kraft@yandex.ru>
2025-12-25 16:07:59 +0300

    Add files via upload

---
+++ beta/bd.js
@@ -0,0 +1,26 @@
+const userDatabase = {
+
+    "user_5sjr50": {
+        username: "user_5sjr50",
+        profileName: "Дизайнер Профи",
+        profileNickname: "_pro",
+        profileStyle: "Дизайн",
+        avatar: "https://i.postimg.cc/K81gkwBL/photo-5226705699080367487-x.jpg",
+        socialLinks: [
+            { platform: "telegram", username: "_pro" },
+            { platform: "instagram", username: "pro" }
+        ],
+        works: [
+            "https://i.postimg.cc/Vk3Hs0tN/photo-5271895704291372799-c.jpg",
+            "https://i.postimg.cc/Vk3Hs0tN/photo-5271895704291372799-c.jpg",
+            "https://i.postimg.cc/Vk3Hs0tN/photo-5271895704291372799-c.jpg",
+            "https://i.postimg.cc/Pf9GVnCH/photo-5244699232638725578-y.jpg",
+            "https://i.postimg.cc/DwQtzWBf/photo-5244699232638725577-y.jpg"
+        ],
+        publishedOn: "design",
+        password: "1234590",
+        profileDescription: "",
+    },
+
+
+}
